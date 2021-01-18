83b47a6dc24b51d5ff3740b098c87c14e62ac5f1
Marco Mengelkoch <Mmengelkoch@gmx.de>
2021-01-18 11:59:14 +0100

    Added msvc sample

--- tests/c_sample.c
+++ tests/c_sample.c
@@ -5,6 +5,11 @@
 // gcc -w -o tests/c_sample.exe tmp_c/*.o -lole32 -lcomctl32 -loleaut32 -luuid -lgdi32 -Itmp_c
 // 
 // nim cpp --threads:on --verbosity:2 -d:debug --debuginfo  --debugger:native -d:useStdLib --noMain --noLinking --header:nimview.h --nimcache=./tmp_c nimview.nim
+
+// gcc -c -w -o tests/c_sample.o -c  -w -fmax-errors=3 -DWEBVIEW_STATIC -DWEBVIEW_IMPLEMENTATION -I/home/marco/.nimble/pkgs/webview-0.1.0/webview -DWEBVIEW_GTK=1 -I/usr/include/gtk-3.0 -I/usr/include/pango-1.0 -I/usr/include/glib-2.0 -I/usr/lib64/glib-2.0/include -I/usr/include/fribidi -I/usr/include/cairo -I/usr/include/pixman-1 -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/uuid -I/usr/include/harfbuzz -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/gio-unix-2.0/ -I/usr/include/atk-1.0 -I/usr/include/at-spi2-atk/2.0 -I/usr/include/at-spi-2.0 -I/usr/include/dbus-1.0 -I/usr/lib64/dbus-1.0/include -I/usr/include/webkitgtk-4.0 -I/usr/include/libsoup-2.4 -pthread -I/usr/include/libxml2 -O3 -fno-strict-aliasing -fno-ident   -I/home/marco/.choosenim/toolchains/nim-1.4.2/lib -I/home/marco/apps/nimvue  -Itmp_c tests/c_sample.c
+// gcc -w -o tests/c_sample tests/c_sample.o -L. -lnimview -lm -lrt -lwebkit2gtk-4.0 -lgtk-3 -lgdk-3 -lpangocairo-1.0 -lpango-1.0 -latk-1.0 -lcairo-gobject -lcairo -lgdk_pixbuf-2.0 -lsoup-2.4 -lgio-2.0 -ljavascriptcoregtk-4.0 -lgobject-2.0 -lglib-2.0    -ldl -Itmp_c 
+
+
 // cp .\generated_c\nimview.h . 
 // cd tests
 // // g++ -o c_sample -Inimcache -IC:/Users/Mmengelkoch/.choosenim/toolchains/nim-1.4.2/lib -L../. -lnimview c_sample.c
@@ -17,10 +22,10 @@
 
 char* appendSomething(char* something) {
     const char* appendString = " modified by C";
-    char* result = malloc(strlen(something) + strlen(appendString) + 1); // +1 for the null-terminator
+    char* result = malloc(strlen(something) + strlen(appendString) + 1); // +1 for the null-terminator, strlen is unchecked! "something" needs 0 termination
     if (result) {
-        strcpy(result, something); // safe
-        strcat(result, appendString); // safe
+        strcpy(result, something); // safe, result just created
+        strcat(result, appendString); // safe, result just created with len
     }
     return result;
 }
