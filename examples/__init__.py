adef52e530d409a9870e94a2e77d69ae6b996ebe
Marco Mengelkoch <Mmengelkoch@gmx.de>
2021-02-22 20:20:47 +0100

    fixed build paths in init scripts

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,4 +1,4 @@
 import sys, os, pathlib
 # print(pathlib.Path(os.path.abspath(__file__)).parent.parent)
-sys.path.append(str(pathlib.Path(os.path.abspath(__file__)).parent.parent) + "/build")
+sys.path.append(str(pathlib.Path(os.path.abspath(__file__)).parent.parent) + "/out")
 import nimview
