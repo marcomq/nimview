8968353caa412ad6c717c660eae65d2d051fda03
Marco Mengelkoch <Mmengelkoch@gmx.de>
2021-02-24 20:30:43 +0100

    python lib building correctly

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 import setuptools
 from setuptools import setup, dist, Extension
-from setuptools.command.install import install
+from setuptools.command.build_ext import build_ext
 from subprocess import check_call
 import os
 from os.path import isfile, join
-from shutil import copy
+from shutil import copy, rmtree
 
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
@@ -14,42 +14,22 @@ with open('README.md', encoding='utf-8') as f:
 this_directory = os.path.abspath(os.path.dirname(__file__))
 targetDir = "nimview"
 
-
-class PostInstallCommand(install):
-    """Pre-installation for installation mode."""
-    def run(self):
-        check_call("nimble pyLib".split())
-        print("install")
-        
-        libFiles = [ "out/nimview.so", "out/nimview.pyd"]
-        for fileName in libFiles:
-            fullFileName = os.path.join(this_directory, fileName)
-            if os.path.isfile(fullFileName):
-                copy(fullFileName, targetDir)
-
-        install.run(self)
-
 # create another nimview subfolder as setup.py is much friendlier if you do so
-os.makedirs("nimview", exist_ok=True)
+try:
+    rmtree(targetDir)
+except:
+    pass
+os.makedirs(targetDir, exist_ok=True)
 srcFiles = [ "nimview.nim", "nimview.nimble", "backend-helper.js", "LICENSE", "README.md"]
 for fileName in srcFiles:
     fullFileName = os.path.join(this_directory, fileName)
     if os.path.isfile(fullFileName):
-        copy(fullFileName, "nimview")
+        copy(fullFileName, targetDir)
 
-with open("nimview/__init__.py", "w") as text_file:
-    text_file.write("from nimview import nimview")
+# with open(targetDir + "/__init__.py", "w") as text_file:
+#    text_file.write("from nimview import nimview")
 
-setup(
-    name="nimview",
-    author="Marco Mengelkoch",
-    description = "A lightwight cross platform UI library for Nim, C, C++ or Python. The main purpose is to simplify creation of Desktop applications based on a HTML/CSS/JS layer that is displayed with Webview.",
-    long_description = long_description,
-    long_description_content_type='text/markdown',
-    version="0.1.0",
-    url="https://github.com/marcomq/nimview",
-    license='MIT',
-    classifiers=[
+classifiers = [
         "Development Status :: 4 - Beta",
         "Natural Language :: English",
         "Operating System :: OS Independent",
@@ -67,17 +47,64 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Software Development"
-    ],
-    # include_package_data=True,
-    # distclass = BinaryDistribution,
-    cmdclass={
-        'install': PostInstallCommand,
-    },
-    packages=["nimview"],
-    # ext_modules=nimporter.build_nim_extensions(exclude_dirs=["tests", "examples"]),
+    ]
+    
+class NimExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
 
+class NimBuild(build_ext):
+    def run(self):
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        print("=> build_extension")
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        
+        extdir = self.get_ext_fullpath(ext.name)
+        if not os.path.exists(extdir):
+            os.makedirs(extdir)
+
+        for fileName in srcFiles:
+            fullFileName = os.path.join(this_directory, fileName)
+            if os.path.isfile(fullFileName):
+                print("copy " + fullFileName + " => " + self.build_temp)
+                copy(fullFileName, self.build_temp)
+
+        check_call(['nimble', 'install', '-d'], cwd=self.build_temp)
+        check_call(['nimble', 'pyLib'], cwd=self.build_temp)
+        libFiles = [ "out/nimview.so", "out/nimview.pyd"]
+        install_target = os.path.abspath(os.path.dirname(extdir))
+
+        for fileName in libFiles:
+            fullFileName = os.path.join(self.build_temp, fileName)
+            if os.path.isfile(fullFileName):
+                print("copy " + fullFileName + " => " + install_target)
+                copy(fullFileName, install_target)
+
+setup(
+    name="nimview",
+    version="0.1.0",
+    author="Marco Mengelkoch",
+    description = "A lightwight cross platform UI library for Nim, C, C++ or Python. The main purpose is to simplify creation of Desktop applications based on a HTML/CSS/JS layer that is displayed with Webview.",
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/marcomq/nimview",
+    license='MIT',
+    classifiers=classifiers,
+    packages=["nimview"],
+    # packages=setuptools.find_packages(exclude=["examples", "tests", "dist", "build", "docs"]),
+    # ext_modules=nimporter.build_nim_extensions(exclude_dirs=["tests", "examples"]),
+    # include_package_data=True,
+    ext_modules=[NimExtension('.')],
+    cmdclass={
+        'build_ext': NimBuild,
+    },
     package_data={
-    	"nimview": ["*.nim*", "*.so", "*.pyd", "nimview.nimble", "backend-helper.js", "LICENSE"]
+    	"nimview": srcFiles + ["nimview.so", "nimview.pyd"]
     },
     install_requires=[
         "choosenim_install"  # Auto-installs Nim compiler
